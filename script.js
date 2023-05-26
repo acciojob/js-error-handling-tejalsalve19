075949eb9c488e44582c61ef7d9d29476eb6eff5
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-05-26 17:01:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //your code here
+class OutOfRangeError extends Error {
+  constructor() {
+    super();
+    this.name = this.constructor.name;
+    this.message = "Expression should only consist of integers and +-/* characters.";
+  }
+}
+
+class InvalidExprError extends Error {
+  constructor() {
+    super();
+    this.name = this.constructor.name;
+    this.message = "Expression should not have an invalid combination of operators.";
+  }
+}
+
+function evalString(expression) {
+  try {
+    if (/[\d+-/*\s]+/.test(expression) === false) {
+      throw new OutOfRangeError();
+    }
+
+    if (/(\+\+|\-\-|\+\-|\-\+|\*\/|\/\*|\+\*|\-\*|\*\+|\*\-|\/\+|\/\-)/.test(expression)) {
+      throw new InvalidExprError();
+    }
+
+    if (/^[\/*+].*/.test(expression)) {
+      throw new SyntaxError("Expression should not start with an invalid operator.");
+    }
+
+    if (/.*[\/*+-]$/.test(expression)) {
+      throw new SyntaxError("Expression should not end with an invalid operator.");
+    }
+
+    // Evaluate the expression here
+    // ...
+
+    return "Expression is valid and can be evaluated.";
+  } catch (error) {
+    console.error(`${error.name}: ${error.message}`);
+  }
+}
+
+// Testing the evaluator
+evalString("2 + 3"); // Valid expression
+evalString("5 / 0"); // OutOfRangeError
+evalString("2 ++ 3"); // InvalidExprError
+evalString("+5 * 4"); // SyntaxError
+evalString("3 -"); // SyntaxError
