3c4f866ef68287d5058e669e40aaff7ded7060db
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-06-29 16:00:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 //your code here
 
+
+
+class OutOfRangeError extends Error {
+  constructor() {
+    super();
+    this.name = this.constructor.name;
+    this.message = "Expression should only consist of integers and +-/* characters.";
+  }
+}
+
+class InvalidExprError extends Error {
+  constructor() {
+    super();
+    this.name = this.constructor.name;
+    this.message = "Expression should not have an invalid combination of operators.";
+  }
+}
+
 function evalString(expression) {
   try {
     if (expression.match(/[^\d\s+\-*/]/)) {
@@ -34,50 +52,6 @@ try {
   evalString('1 / 0');
 } catch (error) {
   console.log(`Caught error: ${error.message}`);
-}
-
-
-class OutOfRangeError extends Error {
-  constructor() {
-    super();
-    this.name = this.constructor.name;
-    this.message = "Expression should only consist of integers and +-/* characters.";
-  }
-}
-
-class InvalidExprError extends Error {
-  constructor() {
-    super();
-    this.name = this.constructor.name;
-    this.message = "Expression should not have an invalid combination of operators.";
-  }
-}
-
-function evalString(expression) {
-  try {
-    if (/[\d+-/*\s]+/.test(expression) === false) {
-      throw new OutOfRangeError();
-    }
-
-    if (/(\+\+|\-\-|\+\-|\-\+|\*\/|\/\*|\+\*|\-\*|\*\+|\*\-|\/\+|\/\-)/.test(expression)) {
-      throw new InvalidExprError();
-    }
-
-    if (/^[\/*+].*/.test(expression)) {
-      throw new SyntaxError("Expression should not start with an invalid operator.");
-    }
-
-    if (/.*[\/*+-]$/.test(expression)) {
-      throw new SyntaxError("Expression should not end with an invalid operator.");
-    }
-
-    // Evaluate the expression here
-    // ...
-
-    return "Expression is valid and can be evaluated.";
-  } catch (error) {
-    console.error(`${error.name}: ${error.message}`);
-  }
 }
 
 // // Testing the evaluator
